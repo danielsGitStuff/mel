24b1d33a5e8f85b1f65f07e54cff5db7b74ed814
daniel <mrkeule@gmail.com>
2017-03-07 19:48:57 +0100

    --transfer

---
+++ app/src/main/java/de/mein/controller/CertificateListAdapter.java
@@ -0,0 +1,38 @@
+package de.mein.controller;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by xor on 3/7/17.
+ */
+
+public class CertificateListAdapter extends BaseAdapter {
+
+    private int count = 0;
+
+    public CertificateListAdapter(){
+
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
