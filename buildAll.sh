b1eea6231d0bd5badf648c58b6f2f5b8c51f435a
daniel <mrkeule@gmail.com>
2018-11-24 01:49:31 +0100

    more update server stuff, build stuff

---
+++ buildAll.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+targetDir="collection"
+rm -rf "$targetDir"
+mkdir "$targetDir"
+if [ "$1" == "clean" ]; then
+    echo "cleaning"
+    ./gradlew clean
+fi
+echo "build server"
+./gradlew :miniserver:buildServerJar
+echo "build fx"
+./gradlew :fxbundle:buildFxJar
+echo "build android"
+./gradlew :app:assembleDebug
+
+cp miniserver/build/libs/* "$targetDir"
+cp fxbundle/build/libs/* "$targetDir"
+cp app/build/outputs/apk/debug/* "$targetDir"
+
+echo "done!"
