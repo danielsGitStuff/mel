8218c817d342e10098597b189f0b003a07831fe7
mrkeule@gmail.com <0nceuponat!me>
2019-01-22 14:36:21 +0100

    added makeshift static web server for riot.im web app. does not belong to this project

---
+++ miniserver/src/main/kotlin/de/miniserver/StaticConfig.kt
@@ -0,0 +1,47 @@
+package de.miniserver
+
+import de.mein.KResult
+import de.mein.auth.data.MeinAuthSettings
+import de.mein.sql.Pair
+import java.io.File
+import java.util.HashMap
+
+class StaticConfig :KResult{
+    var certPath: String? = null
+    var workingPath: String? = DEFAULT_WORKING_DIR.absolutePath
+    var pubKeyPath: String? = null
+    var certName: String? = null
+
+    var privKeyPath: String? = null
+    private val files = HashMap<String, Pair<String>>()
+    var httpPort: Int? = null
+    var httpsPort: Int? = null
+    var authPort: Int = DEFAULT_AUTH
+    var transferPort: Int = DEFAULT_TRANSFER
+    var pipes: Boolean = true
+    var workingDirectory: File? = null
+        get() = File(workingPath)
+    var keySize: Int = 2048
+    var restartCommand = mutableListOf<String>()
+    var keepBinaries: Boolean = false
+
+    var staticPath: String? = null
+
+
+    fun getFiles(): Map<String, Pair<String>> {
+        return files
+    }
+
+    fun addEntry(binaryFile: String, name: String, versionFile: String): StaticConfig {
+        files[binaryFile] = Pair(String::class.java, name, versionFile)
+        return this
+    }
+
+    companion object {
+        val DEFAULT_WORKING_DIR = File("server")
+        val DEFAULT_AUTH: Int = MeinAuthSettings.UPDATE_MSG_PORT
+        const val DEFAULT_TRANSFER: Int = MeinAuthSettings.UPDATE_BINARY_PORT
+        const val DEFAULT_HTTP: Int = 8080
+        const val DEFAULT_HTTPS: Int = 8443
+    }
+}
---
+++ miniserver/src/main/kotlin/de/miniserver/StaticHttpThingy.kt
@@ -0,0 +1,77 @@
+package de.miniserver
+
+import com.sun.net.httpserver.HttpServer
+import de.mein.DeferredRunnable
+import de.mein.Lok
+import de.mein.MeinThread
+import de.miniserver.MiniServer
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+import java.util.*
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.concurrent.Semaphore
+
+
+class StaticHttpThingy(private val port: Int) : DeferredRunnable() {
+    override fun onShutDown() {
+
+    }
+
+    override fun runImpl() {
+        start()
+    }
+
+    override fun getRunnableName(): String = "HTTP"
+
+
+    private lateinit var serverSocket: ServerSocket
+    private val threadSemaphore = Semaphore(1, true)
+    private val threadQueue = LinkedList<MeinThread>()
+
+    private lateinit var server: HttpServer
+    private val threadFactory = { r: Runnable ->
+        var meinThread: MeinThread? = null
+
+        try {
+            threadSemaphore.acquire()
+            meinThread = threadQueue.poll()
+            threadSemaphore.release()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        meinThread
+    }
+    private val executor: Executor = Executors.newFixedThreadPool(2)
+
+    private lateinit var pageBytes: ByteArray
+
+
+    fun start() {
+        Lok.debug("binding http to           : $port")
+        server = HttpServer.create(InetSocketAddress(port), 0)
+        Lok.debug("successfully bound http to: $port")
+        // create the index page context
+        server.createContext("/") {
+            val enteredHost = it.requestHeaders.getFirst("Host")
+            it.responseHeaders.add("Location", "https://$enteredHost")
+            with(it) {
+                Lok.debug("sending https redirect to $remoteAddress")
+//                val content = "???"
+                sendResponseHeaders(301, -1)
+//                responseBody.write(content.toByteArray())
+                responseBody.close()
+            }
+        }
+
+        server.executor = executor
+        server.start()
+        Lok.debug("http is up")
+    }
+
+    fun stop() {
+        server.stop(0)
+    }
+
+}
