ec29b8a18005013c1df827d580db27c4df1f9712
mrkeule@gmail.com <0nceuponat!me>
2019-02-03 20:47:39 +0100

    messenger bootloader

--- messenger/src/main/kotlin/de/mein/msg/MessengerService.kt
+++ messenger/src/main/kotlin/de/mein/msg/MessengerService.kt
@@ -1,7 +1,50 @@
 package de.mein.msg
 
-import de.mein.auth.service.MeinService
+import de.mein.auth.MeinNotification
+import de.mein.auth.data.IPayload
+import de.mein.auth.data.db.Certificate
+import de.mein.auth.jobs.Job
+import de.mein.auth.service.MeinAuthService
+import de.mein.auth.service.MeinServiceWorker
+import de.mein.auth.socket.process.`val`.Request
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
 
-class MessengerService  {
+class MessengerService(meinAuthService: MeinAuthService, workingDir: File, typeId: Long, uuid: String) : MeinServiceWorker(meinAuthService, workingDir, typeId, uuid) {
+    override fun workWork(job: Job<*, *, *>?) {
+    }
+
+    override fun handleCertificateSpotted(partnerCertificate: Certificate?) {
+    }
+
+    override fun createSendingNotification(): MeinNotification {
+        return MeinNotification(uuid,"none","No title","not text")
+    }
+
+    override fun onCommunicationsDisabled() {
+    }
+
+    override fun createExecutorService(threadFactory: ThreadFactory?): ExecutorService {
+        return Executors.newCachedThreadPool()
+    }
+
+    override fun onCommunicationsEnabled() {
+    }
+
+    override fun onMeinAuthIsUp() {
+
+    }
+
+    override fun connectionAuthenticated(partnerCertificate: Certificate?) {
+    }
+
+    override fun handleMessage(payload: IPayload?, partnerCertificate: Certificate?, intent: String?) {
+    }
+
+    override fun handleRequest(request: Request<*>?) {
+
+    }
 
 }
--- messenger/src/main/kotlin/de/mein/msg/MsgBootloader.kt
+++ messenger/src/main/kotlin/de/mein/msg/MsgBootloader.kt
@@ -3,8 +3,12 @@ package de.mein.msg
 import de.mein.auth.data.db.Service
 import de.mein.auth.service.BootLoader
 import de.mein.auth.service.MeinAuthService
+import org.jdeferred.Deferred
+import org.jdeferred.DeferredManager
 import org.jdeferred.Promise
+import org.jdeferred.impl.DefaultDeferredManager
 import org.jdeferred.impl.DeferredObject
+import java.io.File
 import java.lang.Exception
 
 class MsgBootloader : BootLoader() {
@@ -12,7 +16,16 @@ class MsgBootloader : BootLoader() {
 
     override fun getDescription(): String = "messages"
 
-    override fun boot(meinAuthService: MeinAuthService?, services: MutableList<Service>?): Promise<Void, Exception, Void> {
-        return DeferredObject();
+    override fun boot(meinAuthService: MeinAuthService, services: MutableList<Service>): Promise<Void, Exception, Void> {
+        val booted = DeferredObject<Void, Exception, Void>()
+        val deferredManager = DefaultDeferredManager()
+        val promises = mutableListOf<Deferred<Void, Exception, Void>>()
+        for (service in services) {
+            val workingDir = File(bootLoaderDir, service.uuid.v())
+            val messenger = MessengerService(meinAuthService, workingDir, service.typeId.v(), service.uuid.v())
+            promises + messenger.startedDeferred
+        }
+        deferredManager.`when`(*promises.toTypedArray()).done { booted.resolve(null) }.fail { result -> booted.reject(result.reject as Exception?) }
+        return booted
     }
 }
