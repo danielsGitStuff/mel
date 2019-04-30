7ffad44889eb0c78ff3c3c4866554f1f93f9839f
mrkeule@gmail.com <0nceuponat!me>
2019-04-30 22:26:44 +0200

    fiddling with github & web server

--- miniserver/src/main/kotlin/de/miniserver/http/HttpsThingy.kt
+++ miniserver/src/main/kotlin/de/miniserver/http/HttpsThingy.kt
@@ -106,6 +106,7 @@ class HttpsThingy(private val port: Int, private val miniServer: MiniServer, pri
             if (it.requestMethod == "POST") {
                 val values = readPostValues(it)
                 val pw = values["pw"]
+                Lok.debug("##  pw: $pw")
                 val arguments = it.requestURI.path.substring("/private/loggedIn".length).split(" ")
                 val targetPage = values["target"]
                 when (pw) {
