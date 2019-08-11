aac69ef7d1db4e5f34571ddde1934e2ad30518fd
mrkeule@gmail.com <0nceuponat!me>
2019-08-11 18:02:44 +0200

    miniserver fixes

--- serverparts/src/main/kotlin/de/mein/serverparts/AbstractHttpsThingy.kt
+++ serverparts/src/main/kotlin/de/mein/serverparts/AbstractHttpsThingy.kt
@@ -49,7 +49,11 @@ abstract class AbstractHttpsThingy(private val port: Int, val sslContext: SSLCon
 
         meinThread
     }
-    open val executor = Executors.newFixedThreadPool(2)
+    open val executor = Executors.newFixedThreadPool(2) {
+        val thread = Thread(it)
+        thread.name = "a HTTPS-Thread"
+        thread
+    }
 
 //            = Executors.newCachedThreadPool(ThreadFactory {
 //        val thread = Thread(it)
