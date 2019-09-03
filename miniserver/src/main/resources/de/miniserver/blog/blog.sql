0fe0fe911fe8743ab7e2002a78c706b2e566f7b2
mrkeule@gmail.com <0nceuponat!me>
2019-09-03 03:33:49 +0200

    small fixies

--- miniserver/src/main/resources/de/miniserver/blog/blog.sql
+++
@@ -1,8 +0,0 @@
-create table  if not exists blogentry
-(
-    id integer not null  primary key autoincrement,
-    title text,
-    text text not null,
-    tstamp integer not null,
-    published integer not null default 0
-);
