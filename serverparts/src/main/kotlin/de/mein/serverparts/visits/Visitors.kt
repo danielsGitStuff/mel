4b40d8ff398795f70d8563374aafd0825bf0b1ae
mrkeule@gmail.com <0nceuponat!me>
2019-09-21 01:20:22 +0200

    dumpfx refined, Bootloaders sorted when creating a new one

--- serverparts/src/main/kotlin/de/mein/serverparts/visits/Visitors.kt
+++ serverparts/src/main/kotlin/de/mein/serverparts/visits/Visitors.kt
@@ -15,7 +15,7 @@ class Visitors(val dao: VisitsDao) {
         if (ex.requestMethod == "GET") {
             val now = LocalDateTime.now()
             // day = 20190324
-            val day = "${now.year}${if (now.monthValue > 0) now.monthValue else "0${now.monthValue}"}${if (now.dayOfMonth > 9) now.dayOfMonth else "0${now.dayOfMonth}"}".toInt()
+            val day = "${now.year}${if (now.monthValue > 9) "${now.monthValue}" else "0${now.monthValue}"}${if (now.dayOfMonth > 9) "${now.dayOfMonth}" else "0${now.dayOfMonth}"}".toInt()
             val src = ex.remoteAddress.hostName.toString()
             val target = ex.requestURI.toString()
             dao.increase(day, src, target)
