8f9b25ed5d0f28821d12c53ead70c22d7e9d2388
mrkeule@gmail.com <0nceuponat!me>
2019-09-26 03:30:03 +0200

    removed rubbish, visitor count modification

--- serverparts/src/main/kotlin/de/mel/serverparts/visits/Visitors.kt
+++ serverparts/src/main/kotlin/de/mel/serverparts/visits/Visitors.kt
@@ -16,7 +16,7 @@ class Visitors private constructor(private val dao: VisitsDao) {
             val now = LocalDateTime.now()
             // day = 20190324
             val day = "${now.year}${if (now.monthValue > 9) "${now.monthValue}" else "0${now.monthValue}"}${if (now.dayOfMonth > 9) "${now.dayOfMonth}" else "0${now.dayOfMonth}"}".toInt()
-            val src = ex.remoteAddress.hostName.toString()
+            val src = ex.remoteAddress.toString()
             val target = ex.requestURI.toString()
             dao.increase(day, src, target)
         }
