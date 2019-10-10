7d921d733ef679611d83942091b7be410b9f66b7
xor <mrkeule@gmail.com>
2019-10-10 22:39:36 +0200

    could not pair with dump service on android

--- app/src/main/java/de/mel/android/filedump/controller/RemoteDumpServiceChooserGuiController.kt
+++ app/src/main/java/de/mel/android/filedump/controller/RemoteDumpServiceChooserGuiController.kt
@@ -3,7 +3,13 @@ package de.mel.android.filedump.controller
 import android.view.ViewGroup
 import de.mel.android.MainActivity
 import de.mel.android.filesync.controller.RemoteFileSyncServiceChooserGuiController
+import de.mel.auth.data.db.ServiceJoinServiceType
 import de.mel.auth.service.MelAuthService
+import de.mel.dump.DumpStrings
+import de.mel.filesync.data.FileSyncStrings
 
 class RemoteDumpServiceChooserGuiController(melAuthService: MelAuthService, activity: MainActivity, viewGroup: ViewGroup) : RemoteFileSyncServiceChooserGuiController(melAuthService, activity, viewGroup) {
+    override fun showService(service: ServiceJoinServiceType?): Boolean {
+        return service!!.type.v() == DumpStrings.NAME && service.additionalServicePayload != null
+    }
 }
