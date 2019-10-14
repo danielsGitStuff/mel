0c8440cfee9facb47634e9a8efabebb748d3a29a
mrkeule@gmail.com <0nceuponat!me>
2019-10-14 19:37:53 +0200

    WIP: MelRequest timeout

---
+++ auth/src/main/java/de/mel/auth/data/MelRequest.kt
@@ -0,0 +1,119 @@
+package de.mel.auth.data
+
+import de.mel.Lok
+import de.mel.auth.MelStrings.msg
+import de.mel.auth.data.db.Certificate
+import de.mel.auth.socket.MelSocket
+import de.mel.core.serialize.JsonIgnore
+import de.mel.core.serialize.SerializableEntity
+import org.jdeferred.impl.DeferredObject
+import java.security.SecureRandom
+
+/**
+ * Created by xor on 4/28/16.
+ */
+class MelRequest() : MelMessage() {
+    var requestId: Long = SecureRandom().nextLong()
+        private set
+    var answerId: Long? = null
+        private set
+    @JsonIgnore
+    private var requestHandler: IRequestHandler? = null
+    @JsonIgnore
+    val answerDeferred: DeferredObject<SerializableEntity, ResponseException, Void> = DeferredObject()
+    var secret: ByteArray? = null
+        private set
+    var decryptedSecret: String? = null
+        private set
+    var userUuid: String? = null
+        private set
+    var authenticated: Boolean? = null
+        private set
+    var certificate: Certificate? = null
+        private set
+    /**
+     * see @[MelSocket]
+     */
+    private var mode: String? = null
+
+    constructor(serviceUuid: String?, intent: String?) : this() {
+        this.serviceUuid = serviceUuid
+        this.intent = intent
+    }
+
+    fun setRequestId(requestId: Long): MelRequest {
+        this.requestId = requestId
+        return this
+    }
+
+    fun reponse(): MelResponse {
+        return MelResponse().setResponseId(requestId)
+    }
+
+    fun setRequestHandler(requestHandler: IRequestHandler?): MelRequest {
+        this.requestHandler = requestHandler
+        return this
+    }
+
+    fun queue(): MelRequest {
+        requestHandler!!.queueForResponse(this)
+        return this
+    }
+
+    override fun setPayLoad(payLoad: ServicePayload): MelRequest {
+        return super.setPayLoad(payLoad) as MelRequest
+    }
+
+    fun setAnswerId(answerId: Long): MelRequest {
+        this.answerId = answerId
+        return this
+    }
+
+    fun setSecret(secret: ByteArray): MelRequest {
+        this.secret = secret
+        return this
+    }
+
+    fun setDecryptedSecret(decryptedSecret: String?): MelRequest {
+        this.decryptedSecret = decryptedSecret
+        return this
+    }
+
+    fun setUserUuid(userUuid: String?): MelRequest {
+        this.userUuid = userUuid
+        return this
+    }
+
+    fun setAuthenticated(authenticated: Boolean): MelRequest {
+        this.authenticated = authenticated
+        return this
+    }
+
+    fun setCertificate(certificate: Certificate?): MelRequest {
+        this.certificate = certificate
+        return this
+    }
+
+    fun request(): MelRequest {
+        return MelRequest().setAnswerId(requestId)
+    }
+
+    override fun setState(state: String): MelMessage {
+        return super.setState(state)
+    }
+
+    fun respondError(e: Exception?): MelResponse {
+        val response: MelResponse = reponse().setState(msg.STATE_ERR)
+        if (e == null) return response
+        if (e is ResponseException) response.setException(e) else {
+            response.setException(ResponseException(e))
+        }
+        return response
+    }
+
+    fun setMode(mode: String?) {
+        Lok.debug("")
+        this.mode = mode
+    }
+
+}
---
+++ auth/src/main/java/de/mel/auth/socket/RequestTimer.kt
@@ -0,0 +1,76 @@
+package de.mel.auth.socket
+
+import de.mel.Lok
+import de.mel.auth.data.MelRequest
+import de.mel.auth.data.ResponseException
+import de.mel.sql.RWLock
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicBoolean
+
+class RequestTimer(val request: MelRequest) {
+    private var job: Job? = null
+    val repetitions = 5
+    var repCounter = 0
+    val delay = 1000L
+    var done = AtomicBoolean(false)
+
+    suspend fun doTimerThings(): Unit = withContext(Dispatchers.Default) {
+        job?.cancel()
+        job = launch(Dispatchers.Default) {
+
+            repeat(repetitions) {
+                Lok.debug("waiting $it")
+                delay(1000)
+            }
+            if (!done.get())
+                request.answerDeferred.reject(ResponseException("too long"))
+            Lok.debug("done ")
+        }
+    }
+
+    fun success() {
+        Lok.debug("success")
+        done.set(true)
+        job?.cancel()
+        job = null
+    }
+
+    fun fail(msg: String): Unit {
+        fail(ResponseException(msg))
+    }
+
+    fun fail(e: ResponseException): Unit {
+        Lok.debug("fail")
+        done.set(true)
+        job?.cancel()
+        request.answerDeferred.reject(e)
+        job = null
+    }
+
+    suspend fun restart(): Unit {
+        doTimerThings()
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = runBlocking {
+            launch {
+                val request = MelRequest("test", "intent")
+                request.answerDeferred.done {
+                    Lok.debug("done")
+                }.fail {
+                    Lok.debug("failed successfully")
+                }
+                val timer = RequestTimer(request)
+                launch(Dispatchers.Default) {
+                    timer.doTimerThings()
+                }
+                Lok.debug("wait main")
+                delay(2500L)
+                timer.restart()
+                RWLock().lockWrite().lockWrite()
+            }
+        }
+    }
+}
